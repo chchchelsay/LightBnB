7e1f08a8b9a69222d627caea466b76801cd5a582
Chelsea Dwarika <chelseakristina@live.com>
2022-12-10 03:06:37 +0000

    add fake data into all four tables

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,3 +1,5 @@
+-- \i migrations/01_schema.sql
+
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS properties CASCADE;
 DROP TABLE IF EXISTS reservations CASCADE;
@@ -5,10 +7,10 @@ DROP TABLE IF EXISTS property_reviews CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL, 
-  email VARCHAR(255) NOT NULL, 
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
-  );
+);
 
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -34,17 +36,17 @@ CREATE TABLE properties (
 
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
-  start_date DATE NOT NULL, 
-  end_date DATE NOT NULL, 
-  property_id INTEGER NOT NULL REFERENCES properties(id) ON DELETE CASCADE,
-  guest_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE properties_reviews (
+CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
-  guest_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-  property_id INTEGER NOT NULL REFERENCES properties(id) ON DELETE CASCADE,
-  reservation_id INTEGER NOT NULL REFERENCES reservations(id) ON DELETE CASCADE,
-  rating SMALLINT NOT NULL DEFAULT 0, 
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
 );
